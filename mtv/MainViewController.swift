76f5753c163673cb3a3053a1ddda15ebb5165ebd
Ben Guo <ben@venmo.com>
2014-08-16 15:40:29 -0400

    colorAnimation

--- mtv/MainViewController.swift
+++ mtv/MainViewController.swift
@@ -3,13 +3,15 @@ import SceneKit
 
 class MainViewController: NSViewController {
 
+    var sceneView: SCNView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
 
     override func awakeFromNib() {
-        var sceneView: SCNView = self.view as SCNView
+        sceneView = self.view as SCNView
         sceneView.backgroundColor = NSColor.grayColor()
         sceneView.scene = SCNScene()
         let rootNode = sceneView.scene.rootNode
@@ -37,94 +39,42 @@ class MainViewController: NSViewController {
         diffuseLightNode.position = SCNVector3Make(0, 80, 80)
         rootNode.addChildNode(diffuseLightNode)
 
-        let flatShinyMaterial = SCNMaterial()
-        let cubeMap = [
-            NSImage(named: "darkland-right.jpg"),
-            NSImage(named: "darkland-left.jpg"),
-            NSImage(named: "darkland-top.jpg"),
-            NSImage(named: "darkland-bottom.jpg"),
-            NSImage(named: "darkland-back.jpg"),
-            NSImage(named: "darkland-front.jpg"),
-        ]
-        flatShinyMaterial.reflective.contents = cubeMap
-        flatShinyMaterial.shininess = 100
-        flatShinyMaterial.diffuse.contents = NSColor.darkGrayColor()
-        flatShinyMaterial.specular.contents = NSColor.whiteColor()       
+        let flatShinyMaterial = SCNMaterial(cubeMap: "darkland")
 
         let floor = SCNFloor()
-        floor.reflectionFalloffEnd = 100
+        floor.reflectionFalloffEnd = 200
         floor.materials = [flatShinyMaterial]
         let floorNode = SCNNode(geometry: floor)
         rootNode.addChildNode(floorNode)
 
-        // not used
-        let colorAnimation = CAKeyframeAnimation(keyPath: "color")
-        colorAnimation.values = [
-            NSColor.redColor(),
-            NSColor.blueColor(),
-            NSColor.greenColor(),
-            NSColor.redColor()
-        ]
-        colorAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-        colorAnimation.repeatCount = Float.infinity
-        colorAnimation.duration = 3.0
 
-//        SCNNode *armatureNode = [self.scene.rootNode childNodeWithName:@"Armature" recursively:YES];
-//        NSURL *sceneURL = [[NSBundle mainBundle] URLForResource:@"streetlamp-providence" withExtension:@"dae"];
-        let sceneURL = NSBundle.mainBundle().URLForResource("streetlamp-luminaire", withExtension: "dae")
-        let sceneSource = SCNSceneSource(URL: sceneURL, options: nil)
-        let identifiers = sceneSource.identifiersOfEntriesWithClass(SCNNode.self) as [String]
-        let streetlampNode = SCNNode()
-        for id: String in identifiers {
-            let node = sceneSource.entryWithIdentifier(id, withClass: SCNNode.self) as SCNNode
-            streetlampNode.addChildNode(node)
-        }
-        let streetlampRotation = CATransform3DMakeRotation(CGFloat(M_PI/2.0), CGFloat(-1), CGFloat(0), CGFloat(0))
-        streetlampNode.transform = streetlampRotation
-        streetlampNode.position = SCNVector3Make(0, -190, 0)
-        rootNode.addChildNode(streetlampNode)
+        let animalgirlNode = SCNNode(resourceName: "animalgirl")
+        animalgirlNode.rotate(-M_PI/3.0, x: 0, y: 1, z: 0)
+        animalgirlNode.position = SCNVector3Make(0, -190, 0)
+        animalgirlNode.scale(0.1)
+        rootNode.addChildNode(animalgirlNode)
 
-
-//        SCNSceneSource *sceneSource = [SCNSceneSource sceneSourceWithURL:sceneURL options:nil];
-//        CAAnimation *animationObject = [sceneSource entryWithIdentifier:@"idle-animation" withClass:[CAAnimation class]];
-//        [armatureNode addAnimation:animationObject forKey:@"idle-animation"];
-
-        let cube = SCNBox(width: 4.0, height: 4.0, length: 4.0, chamferRadius: 0.0)
+        let cube = SCNBox(size: 80.0)
         cube.materials = [flatShinyMaterial]
         let cubeNode = SCNNode(geometry: cube)
+        cubeNode.addRotation(vector: SCNVector3Make(1.0, 1.0, 1.0), duration: 5.0)
+        flatShinyMaterial.addColorAnimation([
+            NSColor.blueColor(),
+            NSColor.blackColor()
+            ], duration: 3.0, repeatCount: Float.infinity)
         rootNode.addChildNode(cubeNode)
-
-        let cubeAnimation = CABasicAnimation(keyPath: "rotation")
-        cubeAnimation.byValue = NSValue(SCNVector4: SCNVector4Make(1.0, 1.0, 0, 2.0*CGFloat(M_PI)))
-        cubeAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-        cubeAnimation.repeatCount = Float.infinity
-        cubeAnimation.duration = 5.0
-        cubeNode.addAnimation(cubeAnimation, forKey: "rotation")
 
         let sphere = SCNSphere(radius: 0.5)
         let sphereNode = SCNNode(geometry: sphere)
         sphereNode.position = SCNVector3Make(3.0, 0.0, 0.0)
         rootNode.addChildNode(sphereNode)
 
-        let box = SCNBox(width: 80.0, height: 80.0, length: 80.0, chamferRadius: 0.0)
+        let box = SCNBox(size: 80.0)
         let boxNode = SCNNode(geometry: box)
-        let rotationTransform = CATransform3DMakeRotation(CGFloat(M_PI/2.0), CGFloat(1), CGFloat(0), CGFloat(0))
-        boxNode.transform = streetlampRotation
-
+        boxNode.rotate(M_PI/2.0, x: 1, y: 0, z: 0)
         box.materials = [flatShinyMaterial]
+        boxNode.addRotation(vector: SCNVector3Make(1, 0.5, 0), duration: 10.0)
         rootNode.addChildNode(boxNode)
-
-        let boxRotation = CAKeyframeAnimation(keyPath: "transform")
-        boxRotation.values = [
-            NSValue(CATransform3D: CATransform3DRotate(boxNode.transform, 0 * CGFloat(M_PI)/2.0, 1, 0.5, 0)),
-            NSValue(CATransform3D: CATransform3DRotate(boxNode.transform, 1 * CGFloat(M_PI)/2.0, 1, 0.5, 0)),
-            NSValue(CATransform3D: CATransform3DRotate(boxNode.transform, 2 * CGFloat(M_PI)/2.0, 1, 0.5, 0)),
-            NSValue(CATransform3D: CATransform3DRotate(boxNode.transform, 3 * CGFloat(M_PI)/2.0, 1, 0.5, 0)),
-            NSValue(CATransform3D: CATransform3DRotate(boxNode.transform, 4 * CGFloat(M_PI)/2.0, 1, 0.5, 0))
-        ]
-        boxRotation.duration = 10.0
-        boxRotation.repeatCount = Float.infinity
-        boxNode.addAnimation(boxRotation, forKey: "transform")
 
         let text = SCNText(string: "☛", extrusionDepth: 4.0)
         let textNode = SCNNode(geometry: text)
---
+++ mtv/SCNNode.swift
@@ -0,0 +1,49 @@
+import SceneKit
+
+extension SCNNode {
+    convenience init(resourceName: String) {
+        self.init()
+        let sceneURL = NSBundle.mainBundle().URLForResource(resourceName, withExtension: "dae")
+        let sceneSource = SCNSceneSource(URL: sceneURL, options: nil)
+        let identifiers = sceneSource.identifiersOfEntriesWithClass(SCNNode.self) as [String]
+        for id: String in identifiers {
+            let node = sceneSource.entryWithIdentifier(id, withClass: SCNNode.self) as SCNNode
+            self.addChildNode(node)
+        }
+    }
+
+    func scale(scale: Float) {
+        let s = CGFloat(scale)
+        let transform = CATransform3DMakeScale(s, s, s)
+        self.transform = transform
+    }
+
+    func rotate(angle: Double, x: Float, y: Float, z: Float) {
+        let rotation = CATransform3DMakeRotation(CGFloat(angle),
+            CGFloat(x), CGFloat(y), CGFloat(z))
+        self.transform = rotation
+    }
+
+    func addRotation(vector v: SCNVector3, duration: Double, repeatCount: Float = Float.infinity) {
+
+        let t = CGFloat(M_PI/2.0)
+        let animation = CAKeyframeAnimation(keyPath: "transform")
+        let transform = self.transform
+        animation.values = [
+            NSValue(CATransform3D: CATransform3DRotate(transform,
+                0.0 * t, v.x, v.y, v.z)),
+            NSValue(CATransform3D: CATransform3DRotate(transform,
+                1.0 * t, v.x, v.y, v.z)),
+            NSValue(CATransform3D: CATransform3DRotate(transform,
+                2 * t, v.x, v.y, v.z)),
+            NSValue(CATransform3D: CATransform3DRotate(transform,
+                3 * t, v.x, v.y, v.z)),
+            NSValue(CATransform3D: CATransform3DRotate(transform,
+                4 * t, v.x, v.y, v.z))
+        ]
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.duration = duration
+        animation.repeatCount = repeatCount
+        self.addAnimation(animation, forKey: "rotation")
+    }
+}
