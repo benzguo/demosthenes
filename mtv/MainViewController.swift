c4e509a73d2b26c66eeb162d38cef8bc69dd3f32
Ben Guo <ben@venmo.com>
2014-08-20 20:18:37 -0400

    skeleton complete

--- mtv/MainViewController.swift
+++ mtv/MainViewController.swift
@@ -116,6 +116,49 @@ class MainViewController: NSViewController, SCNSceneRendererDelegate {
             case .Organ:
                 Organ()
 
+            case .FirstHits1, .FirstHits2, .FirstHits3, .FirstHits4, .FirstHits5:
+                break
+
+            case .Intro1_1, .Intro1_2, .Intro1_3, .Intro1_4, .Intro2_1, .Intro2_2, .Intro2_3, .Intro2_4:
+                break
+
+            case .FirstVerse1_1, .FirstVerse1_2, .FirstVerse2_1, .FirstVerse2_2:
+                break
+
+            case .FirstChorus1_1, .FirstChorus1_2, .FirstChorus1_3, .FirstChorus1_4,
+            .FirstChorus2_1, .FirstChorus2_2, .FirstChorus2_3, .FirstChorus2_4:
+                break
+
+            case .SecondHits1, .SecondHits2, .SecondHits3, .SecondHits4, .SecondHits5:
+                break
+
+            case .SecondVerse1_1, .SecondVerse1_2, .SecondVerse1_3, .SecondVerse1_4,
+            .SecondVerse2_1, .SecondVerse2_2, .SecondVerse2_3, .SecondVerse2_4,
+            .SecondVerse2_5, .SecondVerse2_6, .SecondVerse2_7, .SecondVerse2_8:
+                break
+
+            case .Bridge1_1, .Bridge1_2, .Bridge1_3, .Bridge1_4,
+            .Bridge2_1, .Bridge2_2, .Bridge2_3, .Bridge2_4:
+                break
+
+            case .ThirdHits1, .ThirdHits2, .ThirdHits3, .ThirdHits4, .ThirdHits5:
+                break
+
+            case .Buildup1, .Buildup2, .Buildup3, .Buildup4, .Buildup5:
+                break
+
+            case .BassSolo:
+                break
+
+            case .LastChorus1_1, .LastChorus1_2, .LastChorus1_3, .LastChorus1_4,
+            .LastChorus2_1, .LastChorus2_2, .LastChorus2_3, .LastChorus2_4,
+            .LastChorus3_1, .LastChorus3_2, .LastChorus3_3, .LastChorus3_4,
+            .LastChorus4_1, .LastChorus4_2, .LastChorus4_3, .LastChorus4_4:
+                break
+
+            case .LastHits1, .LastHits2, .LastHits3, .LastHits4, .LastHits5:
+                break
+
             default:
                 break
             }
